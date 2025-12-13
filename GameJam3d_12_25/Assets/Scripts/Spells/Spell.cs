05789e4b55e5f8aca852d5275365273f47ee58d2
Daniel Stempfle <stempfledaniel@gmx.de>
2025-12-13 15:11:19 +0100

    finish shockwave

--- GameJam3d_12_25/Assets/Scripts/Spells/Spell.cs
+++ GameJam3d_12_25/Assets/Scripts/Spells/Spell.cs
@@ -11,7 +11,6 @@ public class Spell : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
