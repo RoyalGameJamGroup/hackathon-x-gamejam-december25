a32cec8a0c3974e686841276c6ec2954d4ca772f
Johannes <mj0st.jo@gmail.com>
2025-12-14 01:39:59 +0100

    add nuke

--- GameJam3d_12_25/Assets/Scripts/Spells/16Nuclear/Nuclear.cs
+++ GameJam3d_12_25/Assets/Scripts/Spells/16Nuclear/Nuclear.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class Nuclear : Spell
 {
     [SerializeField] float distance = 5f;
-    [SerializeField] float height = 7f;
+    [SerializeField] float height = 40f;
+
+    [SerializeField] GameObject model;
 
     Vector3 targetPosition;
-    bool hasLanded = false;
+    bool falling = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,26 +18,27 @@ public class Nuclear : Spell
         transform.position = new Vector3(targetPosition.x, height, targetPosition.z);
 
         PlaySpawnsSound();
+        StartCoroutine(WaitForFall());
+    }
+
+    IEnumerator WaitForFall()
+    {
+        yield return new WaitForSeconds(11.5f);
+        falling = true;
+        model.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 10f * Time.deltaTime);
-        if (!hasLanded && transform.position == targetPosition)
-        {
-            PlayImpactSound();
-            hasLanded = true;
+        if (falling){
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 10f * Time.deltaTime);
         }
     }
 
     // Called when the collider other enters the trigger
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("CarDrop hit " + other.gameObject.name);
-        if (other.gameObject.CompareTag("Enemy") && !hasLanded)
-        {
-            Destroy(other.gameObject);
-        }
+        Debug.Log("GAME HAS ENDED");
     }
 }
